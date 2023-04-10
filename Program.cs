e221fa464c6354e6ecd8a48116cbf0e318efe8c9
kgauthier34 <122947839+kgauthier34@users.noreply.github.com>
2023-04-10 12:35:02 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿// MIS 3013 April 10, 2023
+// HW7 Array
+// Kyle Gauthier 113502872
+
+string[] nameArray;
+nameArray = new string[10];
+
+double[] balanceArray;
+balanceArray = new double[10];
+
+string m;
+
+for(int i = 0; i < nameArray.Length; i++)
+{
+    string s = string.Format($"Input the clients' {i+1} name: ");
+    Console.Write( s );
+    string name = Console.ReadLine();
+    nameArray[i] = name;
+
+    s = string.Format($"Input the clients' {i+1} balance: ");
+    Console.Write(s);
+    string balanceStr=Console.ReadLine();
+    double balanceDbl=Convert.ToDouble(balanceStr);
+
+    balanceArray[i] = balanceDbl;
+}
+
+Console.WriteLine("\nThe clients' names:");
+for (int i = 0; i < nameArray.Length; i++)
+{
+    Console.WriteLine(nameArray[i]);
+}
+
+Console.WriteLine("\nInput a clients' name: ");
+string searchName = Console.ReadLine();
+for(int i=0;i<nameArray.Length ;i++ )
+{
+    if (nameArray[i] == searchName)
+    {
+        m = string.Format($"{searchName} has a balance of {balanceArray[i]:C2}");
+        Console.WriteLine(m); 
+    }
+}
